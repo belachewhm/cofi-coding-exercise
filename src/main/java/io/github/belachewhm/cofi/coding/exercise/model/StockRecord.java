daea567e92bb47c102598eb09e68819f50ef5edb
Haile-Mariam, Belachew <belachewhm@gmail.com>
2018-01-11 16:46:34 -0600

    changed most of the primitive types to double in StockRecord.java, and added some initial implementation in StockService.java

--- src/main/java/io/github/belachewhm/cofi/coding/exercise/model/StockRecord.java
+++ src/main/java/io/github/belachewhm/cofi/coding/exercise/model/StockRecord.java
@@ -1,8 +1,9 @@
 package io.github.belachewhm.cofi.coding.exercise.model;
 
-import java.math.BigDecimal;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.Month;
+import java.time.ZoneId;
 import java.util.Date;
 
 import lombok.Data;
@@ -11,35 +12,39 @@ import lombok.Data;
 public class StockRecord {
 	private String ticker;
 	private Date date;
-	private BigDecimal open;
-	private BigDecimal high;
-	private BigDecimal low;
-	private BigDecimal close;
-	private BigDecimal volume;
-	private BigDecimal ex_dividend;
+	private double open;
+	private double high;
+	private double low;
+	private double close;
+	private double volume;
+	private double ex_dividend;
 	private double split_ratio;
-	private BigDecimal adj_open;
-	private BigDecimal adj_high;
-	private BigDecimal adj_low;
-	private BigDecimal adj_close;
+	private double adj_open;
+	private double adj_high;
+	private double adj_low;
+	private double adj_close;
 	private double adj_volume;
 
 	public StockRecord(String ticker, String date, String open, String high, String low, String close, String volume,
 			String ex_dividend, String split_ratio, String adj_open, String adj_high, String adj_low, String adj_close,
 			String adj_volume) throws ParseException {
-		this.setTicker(ticker);
-		this.setDate(new SimpleDateFormat("yyyy-MM-dd").parse(date));
-		this.setOpen(new BigDecimal(open));
-		this.setHigh(new BigDecimal(high));
-		this.setLow(new BigDecimal(low));
-		this.setClose(new BigDecimal(close));
-		this.setVolume(new BigDecimal(volume));
-		this.setEx_dividend(new BigDecimal(ex_dividend));
-		this.setSplit_ratio(Double.parseDouble(split_ratio));
-		this.setAdj_open(new BigDecimal(adj_open));
-		this.setAdj_high(new BigDecimal(adj_high));
-		this.setAdj_low(new BigDecimal(adj_low));
-		this.setAdj_close(new BigDecimal(adj_close));
-		this.setAdj_volume(Double.parseDouble(adj_volume));
+		setTicker(ticker);
+		setDate(new SimpleDateFormat("yyyy-MM-dd").parse(date));
+		setOpen(Double.parseDouble(open));
+		setHigh(Double.parseDouble(high));
+		setLow(Double.parseDouble(low));
+		setClose(Double.parseDouble(close));
+		setVolume(Double.parseDouble(volume));
+		setEx_dividend(Double.parseDouble(ex_dividend));
+		setSplit_ratio(Double.parseDouble(split_ratio));
+		setAdj_open(Double.parseDouble(adj_open));
+		setAdj_high(Double.parseDouble(adj_high));
+		setAdj_low(Double.parseDouble(adj_low));
+		setAdj_close(Double.parseDouble(adj_close));
+		setAdj_volume(Double.parseDouble(adj_volume));
+	}
+
+	public Month getMonth() {
+		return date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate().getMonth();
 	}
 }
--- src/main/java/io/github/belachewhm/cofi/coding/exercise/service/StockService.java
+++ src/main/java/io/github/belachewhm/cofi/coding/exercise/service/StockService.java
@@ -1,6 +1,9 @@
 package io.github.belachewhm.cofi.coding.exercise.service;
 
+import java.time.Month;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,11 +11,36 @@ import org.springframework.stereotype.Service;
 import io.github.belachewhm.cofi.coding.exercise.model.StockRecord;
 
 @Service
-public class StockService {
+public class StockService
+{
 	@Autowired
 	private List<StockRecord> stockRecords;
 
-	public String averageMonthlyOpenAndClose() {
+	public String averageMonthlyOpenAndClose()
+	{		
+		Map<Month, List<StockRecord>> cofRecordsByMonth = stockRecords.stream().filter(record->record.getTicker().equalsIgnoreCase("COF")).collect(Collectors.groupingBy(StockRecord::getMonth));
+		for(Month month : cofRecordsByMonth.keySet())
+		{
+			List<StockRecord> monthRecords = cofRecordsByMonth.get(month);
+			double average_open = monthRecords.stream().collect(Collectors.averagingDouble(StockRecord::getOpen));
+			double average_close = monthRecords.stream().collect(Collectors.averagingDouble(StockRecord::getClose));
+			
+			System.out.println("Month: " + month);
+			System.out.println("average_open: " + average_open);
+			System.out.println("average_close: " + average_close);
+			System.out.println();
+		}
+		
+		
+		
+		
+		
+		
+		List<StockRecord> googlRecords = stockRecords.stream().filter(record->record.getTicker().equalsIgnoreCase("GOOGL")).collect(Collectors.toList());
+		List<StockRecord> msftRecords = stockRecords.stream().filter(record->record.getTicker().equalsIgnoreCase("MSFT")).collect(Collectors.toList());
+		
+		
+		
 		return String.valueOf(stockRecords.size());
 	}
 
